5dd058b350120c196c4839214745a43971a633c3
laudyeneth <laudyeneth@hotmail.com>
2024-01-02 19:34:18 +0100

    Se agrega la funcionalidad de editar

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ window.onload = function () {
 
     listado.addEventListener("click", eliminarTarea);
     listado.addEventListener("click", terminarTarea);
+    listado.addEventListener("click", editarTarea);
+    listado.addEventListener("click", cancelarEditar);
+    listado.addEventListener("click", guardarEdicionTarea);
     
     //se inicializa id y se carga datos de localStorage
     if (localStorage.length === 0) {
@@ -27,25 +30,40 @@ window.onload = function () {
 
         //Envia los datos del storage a la funcion
         for (const [llave, valor] of arrayLocalS) { 
-            imprimirHtml(llave,JSON.parse(valor));
+            imprimirHtml(llave,valor);
         }
     }
 
     // funcion de agregar tarea 
     function imprimirHtml(id, tarea) {
-        
-        const elemento = `
-                        <li id="elemento">
-                        <button class="terminar" data-id="${id}">✔</button> 
-                        <p>${tarea.completado ? (
-                            `<span class='completa'>${tarea.valor}</span>`
-                        ) : (
-                            `<span class='incompleta'>${tarea.valor}</span>`
-                        )}</p>
-                        <button class="eliminar" data-id="${id}">--</button> 
-                        </li>
-                        `;
-        listado.insertAdjacentHTML("beforeend", elemento);
+
+        try {
+            tarea=JSON.parse(tarea)
+        } catch (error) {
+            console.log("tarea no es JSON " + tarea + error)
+        }
+
+        if(tarea.valor) {
+            const elemento = `
+                            <li id="elemento">
+                            <button class="terminar" id="btn-terminar${id}" data-id="${id}">✔</button> 
+                            <p>${tarea.completado ? (
+                                `<span class='completa' id="valor${id}">${tarea.valor}</span>`
+                            ) : (
+                                `<span class='incompleta' id="valor${id}">${tarea.valor}</span>`
+                            )}</p>
+                            <button class="eliminar" id="btn-eliminar${id}" data-id="${id}">--</button>
+                            <button class="editar" id="btn-editar${id}" data-id="${id}">Editar Tarea</button>
+
+                            <form id="formedit${id}" class="oculto">
+                                <input type="text" id="nuevovalor${id}" data-id="${id}" value="${tarea.valor}" "/>
+                                <button class="guardar" data-id="${id}">Guardar</button>
+                                <button class="cancelar" data-id="${id}" type="button">Cancelar</button>
+                            </form>
+                            </li>
+                            `;
+            listado.insertAdjacentHTML("beforeend", elemento);
+        }
     }
     
     btnAgregar.onclick = function () {
@@ -91,6 +109,71 @@ window.onload = function () {
             localStorage.setItem(tareaID,JSON.stringify(tareaObtenida));
 
             cargarLista();
+        }
+    }
+
+    //editar tarea
+    function editarTarea(e) {
+        if (e.target.classList.contains("editar")) {
+            const tareaID = Number(e.target.getAttribute("data-id"));
+
+            ocultarMostrarBotones(tareaID, true)
+            // muestra formulario de edición de tarea
+            const elemento = document.getElementById('formedit' + tareaID);
+            elemento.className="disponible"
+        }
+    }
+
+    // cancelar edicion
+    function cancelarEditar(e) {
+        if (e.target.classList.contains("cancelar")) {
+            const tareaID = Number(e.target.getAttribute("data-id"));
+
+            ocultarMostrarBotones(tareaID, false)
+            // oculta formulario de edición de tarea
+            const elemento = document.getElementById('formedit' + tareaID);
+            elemento.className="oculto"
+            cargarLista();
+        }
+    }
+
+    function guardarEdicionTarea(e) {
+        if (e.target.classList.contains("guardar")) {
+            const tareaID = Number(e.target.getAttribute("data-id"));
+
+            const nuevoValor = document.getElementById('nuevovalor' + tareaID);
+    
+            let tareaObtenida = JSON.parse(localStorage.getItem(tareaID));
+            tareaObtenida.valor = nuevoValor.value;
+            localStorage.setItem(tareaID,JSON.stringify(tareaObtenida));
+    
+            ocultarMostrarBotones(tareaID, false)
+            // oculta formulario de edición de tarea
+            const elemento = document.getElementById('formedit' + tareaID);
+            elemento.className="oculto"        
+            cargarLista();             
+        }
+
+    }
+
+    function ocultarMostrarBotones(tareaID, ocultar) {
+
+        let nombreClase = 'oculto'
+
+        const btnTerminar = document.getElementById("btn-terminar" + tareaID);
+        const btnEditar = document.getElementById("btn-editar" + tareaID);
+        const btnEliminar = document.getElementById("btn-eliminar" + tareaID);
+        const valor = document.getElementById("valor" + tareaID);
+        if(ocultar) {
+            btnTerminar.classList.add(nombreClase);
+            btnEditar.classList.add(nombreClase);
+            btnEliminar.classList.add(nombreClase);
+            valor.classList.add(nombreClase);
+        } else {
+            btnTerminar.classList.remove(nombreClase);
+            btnEditar.classList.remove(nombreClase);
+            btnEliminar.classList.remove(nombreClase);
+            valor.classList.remove(nombreClase);
         }
     }
 
